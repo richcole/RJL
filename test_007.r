3be77a65ee57d5f4357c52f0f230ff26d0fee958
richcole <richard.j.cole@gmail.com>
2009-12-13 20:29:04 -0800

    Add missing files

---
+++ test_007.r
@@ -0,0 +1,7 @@
+{ sys |
+  i = 0 ;
+  while ( i < 10 ) {
+    sys print "Hello World\n" ;
+    i = i + 1 ;
+  }
+}
---
+++ test_008.r
@@ -0,0 +1,7 @@
+{ sys |
+  i = 0 + 1 ;
+  while ( i < 10 ) {
+    i = i + 2 ;
+    sys print "Hello World\n"
+  }
+}
